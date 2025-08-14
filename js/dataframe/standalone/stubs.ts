3826e01a90e500e1832a88c99d1b9b2c4423ff44
Hannah <hannahblair@users.noreply.github.com>
2025-08-14 13:50:54 +0100

    clean up

--- js/dataframe/standalone/stubs.ts
+++ js/dataframe/standalone/stubs.ts
@@ -14,34 +14,11 @@ export const client = {
 	}
 };
 
-export const format_time = (time: number): string => {
-	const minutes = Math.floor(time / 60);
-	const seconds = Math.floor(time % 60);
-	return `${minutes}:${seconds.toString().padStart(2, "0")}`;
-};
-
-export const skip_audio = (): void => {};
-export const process_audio = (): void => {};
-
 export { default as BaseCheckbox } from "./stubs/BaseCheckbox.svelte";
 export { default as Upload } from "./stubs/Upload.svelte";
 
 export const StatusTracker = null;
-
 export const IconButton = null;
-
-export class ShareError extends Error {
-	constructor(message: string) {
-		super(message);
-		this.name = "ShareError";
-	}
-}
-
-export const reactive_formatter = (key: string, ...args: any[]): string => key;
-
-export const is_browser = typeof window !== "undefined";
-
-export const _ = (key: string, ...args: any[]): string => key;
 
 export { default as FullscreenButton } from "./stubs/FullscreenButton.svelte";
 
